0a04b624a78a0c717efb9e613470185ab23aa2e4
hajnxg <hajnxg@126.com>
2020-07-01 01:39:26 +0800

    init commit

---
+++ src/com/itheima/test01/MainClass.java
@@ -0,0 +1,29 @@
+package com.itheima.test01;
+
+/**
+ * 调试基本介绍
+ * @author TEST
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        int num = 1;
+        System.out.println("num = " + num);
+        num ++;
+        System.out.println("num = " + num);
+        num ++;
+        System.out.println("num = " + num);
+        num ++;
+        System.out.println("num = " + num);
+        num ++;
+        System.out.println("num = " + num);
+        num ++;
+        System.out.println("num = " + num);
+        num ++;
+        System.out.println("num = " + num);
+
+        System.out.println("end...");
+
+    }
+}
+
+
